c6eef21fa50ea51c1fb1145b30813eeafc52413a
unknown <sayedbintaher@gmail.com>
2022-04-17 22:41:55 +0600

    signin signup added

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -1,9 +1,15 @@
 // 1. create a new project in console.firebase.google.com
 // 2. npm install firebase 
 // 3. copy all the components from the firebase sdk and paste in the src folder with the file name firebase.init.json . Also we need to export the default app.
+// 4. We will go firebase settings --> Authentication --> Enable email and password authentication
+// 5. Create Login, Signup component, setup router
+// 6. attach form field handler and form submit handler
+// 7. npm install --save react-firebase-hooks
+// 8. useCreateUserWithEmail from react-firebase-hooks
+// 9. If user is created redirect to desired page
 
 
-//Hosting Steps
+// Hosting Steps
 // 1. npm install -g firebase-tools
 // 2. firebase login and
 // 3. firebase init
